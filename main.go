20b325903e9f99c2a65402aec957753e24983a73
cui <xx@gmail.com>
2025-11-26 19:17:40 +0800

    x

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/shaovie/gutils/ihttp"
+	"github.com/shaovie/gutils/ilog"
+	"github.com/shaovie/gutils/isqlite"
+	"github.com/shaovie/gutils/gutils"
+)
+
+func main() {
+	code, _, _ := ihttp.Get("https://g.cn", 1*time.Second, nil)
+	fmt.Printf("code = %d\n", code)
+	ilog.Init("")
+	isqlite.New("", "")
+	fmt.Printf("random string = %s\n", gutils.RandomString(12))
+}
